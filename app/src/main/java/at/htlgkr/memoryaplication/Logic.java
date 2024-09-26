b43f5cbb5854f4bb55f32ddebf2dc85c27af525b
asecHTL <HumerS22022@sus.htl-grieskirchen.at>
2024-09-26 11:37:59 +0200

    Initialize pictures in Layout

---
+++ app/src/main/java/at/htlgkr/memoryaplication/Logic.java
@@ -0,0 +1,12 @@
+package at.htlgkr.memoryaplication;
+
+import java.util.Random;
+
+public class Logic {
+
+
+    public double getRandomXY(){
+        Random random = new Random();
+        double cords = 00;
+    }
+}
--- app/src/main/java/at/htlgkr/memoryaplication/MainActivity.java
+++ app/src/main/java/at/htlgkr/memoryaplication/MainActivity.java
@@ -1,6 +1,8 @@
 package at.htlgkr.memoryaplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+import org.w3c.dom.Text;
+
+import at.htlgkr.memoryaplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView tvTitle;
+
+    private ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        binding.tvTitle.setText("Test");
+
+        binding.imageView1.setImageResource(R.drawable.picture00);
+        binding.imageView1.setOnClickListener(view -> {
+            binding.imageView1.setImageResource(R.drawable.ic_launcher_background);
+        });
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Snackbar.make(v,"Gewonnen",Snackbar.LENGTH_SHORT).show();
     }
 }
