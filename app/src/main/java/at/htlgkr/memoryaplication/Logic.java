15feb864a9cbdd0de9e3ca26f278888ff628d81c
asecHTL <HumerS22022@sus.htl-grieskirchen.at>
2024-10-02 11:05:47 +0200

    Adding margin to Pictures, Reacting to Image Click

--- app/src/main/java/at/htlgkr/memoryaplication/Logic.java
+++ app/src/main/java/at/htlgkr/memoryaplication/Logic.java
@@ -1,2 +1,43 @@
-package at.htlgkr.memoryaplication;public class Logic {
+package at.htlgkr.memoryaplication;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Logic {
+
+    public List<Integer> fillPicturesSorted(){
+        List<Integer>picturesSorted = new ArrayList<>();
+        //Image Liste befüllen:
+        picturesSorted.add(R.drawable.picture1);
+        picturesSorted.add(R.drawable.picture2);
+        picturesSorted.add(R.drawable.picture3);
+        picturesSorted.add(R.drawable.picture4);
+        picturesSorted.add(R.drawable.picture5);
+        picturesSorted.add(R.drawable.picture6);
+        picturesSorted.add(R.drawable.picture7);
+        picturesSorted.add(R.drawable.picture8);
+        return picturesSorted;
+    }
+
+    public List<Integer> allPicturesInList(){
+        List<Integer>pictures = new ArrayList<>();
+        pictures.add(R.drawable.picture1);pictures.add(R.drawable.picture1);
+        pictures.add(R.drawable.picture2);pictures.add(R.drawable.picture2);
+        pictures.add(R.drawable.picture3);pictures.add(R.drawable.picture3);
+        pictures.add(R.drawable.picture4);pictures.add(R.drawable.picture4);
+        pictures.add(R.drawable.picture5);pictures.add(R.drawable.picture5);
+        pictures.add(R.drawable.picture6);pictures.add(R.drawable.picture6);
+        pictures.add(R.drawable.picture7);pictures.add(R.drawable.picture7);
+        pictures.add(R.drawable.picture8);pictures.add(R.drawable.picture8);
+        return pictures;
+    }
+
+    public List<Integer> fillPicturesRandom(List<Integer>picturesRandom){
+        Collections.shuffle(picturesRandom);
+        return picturesRandom;
+    }
+
+
 }
--- app/src/main/java/at/htlgkr/memoryaplication/MainActivity.java
+++ app/src/main/java/at/htlgkr/memoryaplication/MainActivity.java
@@ -1,6 +1,6 @@
 package at.htlgkr.memoryaplication;
 
-import static at.htlgkr.memoryaplication.Logic.fillGameField;
+
 
 import android.os.Bundle;
 import android.view.View;
@@ -17,15 +17,22 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import at.htlgkr.memoryaplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     TextView tvTitle;
-    Integer[][]gameField;
-    Modell modell;
-    List<Integer>imageViewList = new ArrayList<>();
-    Logic logic;
+    TextView pairCounter;
+    int counter = 0;
+    String pairCounterString = "0";
+
+    List<Integer>picturesSorted = new ArrayList<>();
+    List<Integer>picturesRandom = new ArrayList<>();
+    List<Integer>forGameFieldPictues = new ArrayList<>();
+    List<Integer>picturesOncklick = new ArrayList<>();
+    Logic logic = new Logic();
+
 
     private ActivityMainBinding binding;
 
@@ -42,22 +49,167 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        binding.tvTitle.setText("Test");
 
-        gameField = Logic.fillGameField(imageViewList,gameField);
-
-        //Blanke Felder anzeigen && Pictures in Liste einfügen
+        tvTitle = findViewById(R.id.tvTitle);
+        tvTitle.setText("Let's play some Memory");
+        pairCounter = findViewById(R.id.pairCounter);
+        pairCounterString = String.valueOf(counter);
+        pairCounter.setText(pairCounterString);
         addBlankFieldsToField();
-        addPicturesToList();
-
-        fillGameField(imageViewList,gameField);
-
-        //Neues Modell mit dem Gamefield anlegen
-        modell = new Modell(gameField);
+        //Sortiertes Array befüllen
+        picturesSorted =  logic.fillPicturesSorted();
+        //Alle Gesamten Pictures in einem Array befüllen
+        forGameFieldPictues = logic.allPicturesInList();
+        //Eine gemixte Liste von allen Pictures
+        picturesRandom = logic.fillPicturesRandom(forGameFieldPictues);
 
         binding.imageView1.setOnClickListener(view->{
+            binding.imageView1.setImageResource(picturesRandom.get(0));
+            picturesOncklick.add(picturesRandom.get(0));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView2.setOnClickListener(view->{
+            binding.imageView2.setImageResource(picturesRandom.get(1));
+            picturesOncklick.add(picturesRandom.get(1));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView3.setOnClickListener(view->{
+            binding.imageView3.setImageResource(picturesRandom.get(2));
+            picturesOncklick.add(picturesRandom.get(2));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView4.setOnClickListener(view->{
+            binding.imageView4.setImageResource(picturesRandom.get(3));
+            picturesOncklick.add(picturesRandom.get(3));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView5.setOnClickListener(view->{
+            binding.imageView5.setImageResource(picturesRandom.get(4));
+            picturesOncklick.add(picturesRandom.get(4));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView6.setOnClickListener(view->{
+            binding.imageView6.setImageResource(picturesRandom.get(5));
+            picturesOncklick.add(picturesRandom.get(5));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView7.setOnClickListener(view->{
+            binding.imageView7.setImageResource(picturesRandom.get(6));
+            picturesOncklick.add(picturesRandom.get(6));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView8.setOnClickListener(view->{
+            binding.imageView8.setImageResource(picturesRandom.get(7));
+            picturesOncklick.add(picturesRandom.get(7));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView9.setOnClickListener(view->{
+            binding.imageView9.setImageResource(picturesRandom.get(8));
+            picturesOncklick.add(picturesRandom.get(8));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView10.setOnClickListener(view->{
+            binding.imageView10.setImageResource(picturesRandom.get(9));
+            picturesOncklick.add(picturesRandom.get(9));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView11.setOnClickListener(view->{
+            binding.imageView11.setImageResource(picturesRandom.get(10));
+            picturesOncklick.add(picturesRandom.get(10));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView12.setOnClickListener(view->{
+            binding.imageView12.setImageResource(picturesRandom.get(11));
+            picturesOncklick.add(picturesRandom.get(11));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView13.setOnClickListener(view->{
+            binding.imageView13.setImageResource(picturesRandom.get(12));
+            picturesOncklick.add(picturesRandom.get(12));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView14.setOnClickListener(view->{
+            binding.imageView14.setImageResource(picturesRandom.get(13));
+            picturesOncklick.add(picturesRandom.get(13));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView15.setOnClickListener(view->{
+            binding.imageView15.setImageResource(picturesRandom.get(14));
+            picturesOncklick.add(picturesRandom.get(14));
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        binding.imageView16.setOnClickListener(view->{
+            binding.imageView16.setImageResource(picturesRandom.get(15));
+            picturesOncklick.add(picturesRandom.get(15));
+
+            try {
+                handle(picturesOncklick);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 
         });
+
     }
 
 
@@ -88,20 +240,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    public void addPicturesToList(){
-        //Image Liste befüllen:
-        imageViewList.add(R.drawable.picture1);imageViewList.add(R.drawable.picture1);
-        imageViewList.add(R.drawable.picture2);imageViewList.add(R.drawable.picture2);
-        imageViewList.add(R.drawable.picture3);imageViewList.add(R.drawable.picture3);
-        imageViewList.add(R.drawable.picture4);imageViewList.add(R.drawable.picture4);
-        imageViewList.add(R.drawable.picture4);imageViewList.add(R.drawable.picture5);
-        imageViewList.add(R.drawable.picture5);imageViewList.add(R.drawable.picture6);
-        imageViewList.add(R.drawable.picture6);imageViewList.add(R.drawable.picture7);
-        imageViewList.add(R.drawable.picture7);imageViewList.add(R.drawable.picture8);
+    public void handle(List<Integer>picturesClicked ) throws InterruptedException {
+        if (picturesClicked.size() == 2){
+            if (Objects.equals(picturesClicked.get(0), picturesClicked.get(1))){
+                tvTitle.setText("Toll du hast ein Paar gefunden");
+                Thread.sleep(600);
+                addBlankFieldsToField();
+                pairCounterString = String.valueOf(counter);
+                pairCounter.setText(pairCounterString);
+                picturesClicked.clear();
+                tvTitle.setText("Lets try again");
+
+            }else {
+                tvTitle.setText("Das war woll ein Griff ins kloo!");
+                addBlankFieldsToField();
+                tvTitle.setText("Lets try again");
+                Thread.sleep(600);
+            }
+        }
     }
 
 
-    public void showPictures(Integer[][]pictureList){
 
-    }
+
+
+
+
+
+
 }
